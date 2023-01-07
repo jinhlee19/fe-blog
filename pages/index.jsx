0a9d13b8335192fc0fd69ee24c5ea03081c9344c
jinhlee19 <jinhlee19@gmail.com>
2023-01-08 02:04:21 +0900

    font add

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,26 @@
 import Layout from "@/components/Layout";
+import Image from "next/image";
 
 export default function LandingPage() {
   return (
     <Layout>
-      <section className="content-list grid-cols-4">
+      <section className="content-list grid grid-rows-4 text-center">
         <article className="card space-y-4 border border-text-lighter w-72">
-          <h4 className="card__subtitle">카테고리</h4>
+          <figure>
+            <Image
+              src="/asset/image/sample/sample28.jpg"
+              width={300}
+              height={400}
+              alt="."
+            ></Image>
+          </figure>
+          <h4 className="card__subtitle text-sky-500">카테고리</h4>
           <h2 className="card__title">
             The New Autumn Denim Guide: The Best Gift For Stylish Women
           </h2>
-          <hr className="divider--sm w-8" />
+          <div className="flex justify-center pb-4">
+            <hr className="divider--sm w-8" />
+          </div>
           <time>Monday, June 20, 2022</time>
         </article>
       </section>
