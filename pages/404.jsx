0f47152a1163755d75f60f1824e6bcace091d7c8
jinhlee19 <jinhlee19@gmail.com>
2023-01-08 04:40:57 +0900

    card faker

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,12 @@
 import Layout from "@/components/Layout";
 
 export default function NotFoundPage() {
-  return <Layout></Layout>;
+  return (
+    <Layout title="Page Not Found">
+      <div>
+        <h1>404</h1>
+        <h2>Sorry, there is nothing here.</h2>
+      </div>
+    </Layout>
+  );
 }
---
+++ pages/api/blogs/index.js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { blogs } = require("./data.json");
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req, res) => {
+  if (req.method === "GET") {
+    res.status(200).json(blogs);
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
+  }
+};
